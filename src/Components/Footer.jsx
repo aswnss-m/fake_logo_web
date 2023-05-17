e9c28fe21ef5bc91461139572c5243e87e31d381
aswnss-m <aswnss.m@gmail.com>
2023-05-17 18:07:51 +0530

    firebase hosting init

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+  return (
+    <div className='footerContainer'>
+      <div className="footer">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Footer

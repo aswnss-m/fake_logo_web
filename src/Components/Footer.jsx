2647f4f341244c6c2db160ae30f2858c4d619b23
aswnss-m <aswnss.m@gmail.com>
2023-05-18 07:33:41 +0530

    added the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Footer.css"
 function Footer() {
   return (
     <div className='footerContainer'>
       <div className="footer">
-        
+        <div className="teamMembers">
+          <p className="bold">Team</p>
+          <p>
+            <Link to={"www.linkedin.com/in/aswnss"}>Aswin Lal M | SCT20AM020</Link>
+          </p>
+          <p>
+            <Link to={"www.linkedin.com/in/aswnss"}>Bhagavath Mohan | SCT20AM025</Link>
+          </p>
+          <p>
+            <Link to={"https://www.linkedin.com/in/hashir-bin-anwer-137503205/"}>Hashir Bin Anwer | SCT20AM035</Link>
+          </p>
+        </div>
+        <div className="resources">
+          <p className="bold">Resources</p>
+          <Link to={'/'}>Github</Link>
+        </div>
+        <div className="quickLinks">
+          <p className="bold">Quick Links</p>
+          <p>Home</p>
+          <p>About</p>
+        </div>
       </div>
     </div>
   )

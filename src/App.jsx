28ee9667722dc06991ad486d1e87e2971023a85e
aswnss-m <aswnss.m@gmail.com>
2023-05-18 07:36:01 +0530

    new App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import './assets/App.css'
-import Home from './Home'
-import Navbar from './Components/Navbar'
+import './assets/App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import DragDrop from './Components/DragDrop';
+import Footer from './Components/Footer';
+// import { useState } from 'react';
+import Query from './Query';
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
+
+
   return (
     <>
-    <Navbar />
-    <Home />
+      <Navbar />
+      <Hero />
+      <Routes> 
+        <Route path={'/'} element={<DragDrop />} />
+        <Route path={'/query'} element={<Query />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

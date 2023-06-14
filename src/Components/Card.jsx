647242bf1d3adf59180b039054d7ee3c3bf1bf9e
aswnss-m <aswnss.m@gmail.com>
2023-06-14 09:59:45 +0530

    props added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import logo from "../assets/logo.png"
-import "./Card.css"
+import React from 'react';
+import "./Card.css";
 
-function Card() {
+function Card({ imageSrc, imageName, similarity }) {
   return (
-        <div className="card">
-            <div className="cardImage"><img src={logo} alt="Logo" /><p className='cardImageName'>Name</p></div>
-            <div className="cardSimilarity">
-            <div className="loadingBar bold" number-value={`${50}%`} style={{'--width-value':`${50}%`}}></div>
-            </div> 
-        </div>
-  )
+    <div className="card">
+      <div className="cardImage">
+        <img src={imageSrc} alt="Logo" />
+        <p className='cardImageName'>{imageName}</p>
+      </div>
+      <div className="cardSimilarity">
+        <div className="loadingBar bold" number-value={`${similarity}%`} style={{'--width-value': `${similarity}%`}}></div>
+      </div> 
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/Query.jsx
+++ src/Query.jsx
@@ -1,17 +1,26 @@
 // Query.js
 import React from 'react';
 import Card from './Components/Card';
-import "./assets/Query.css"
+import "./assets/Query.css";
 import QueryHeader from './Components/QueryHeader';
 
 function Query() {
-  const cards = Array.from({ length: 10 }, (_, index) => (
-    <Card key={index}/>
+  // Read data from sessionStorage
+  const resultData = JSON.parse(sessionStorage.getItem('results')) || {};
+  const { file_id, message, result } = resultData;
+
+  // Generate Card components based on the result object
+  const cards = Object.entries(result).map(([filename, score]) => (
+    <Card
+      key={file_id}
+      name={filename}
+      value={score}
+    />
   ));
 
   return (
     <>
-      <QueryHeader/>
+      <QueryHeader />
       <div className="queryContainer">
         {cards}
       </div>
